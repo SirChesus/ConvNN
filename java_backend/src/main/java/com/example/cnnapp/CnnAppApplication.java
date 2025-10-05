b250c10cb7936cfc040268b1948f89aa1f54ee17
mkattela <mayankattela@gmail.com>
2025-10-05 16:59:13 -0700

    syntax/naming fixes

--- java_backend/src/main/java/com/example/cnnapp/CnnAppApplication.java
+++ java_backend/src/main/java/com/example/cnnapp/CnnAppApplication.java
@@ -3,6 +3,7 @@ package com.example.cnnapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CnnAppApplication
 {
     public static void main(String[] args)
